f51e27e4266bc5f008abea24eb603475f96a1173
Jonathan <joth10@live.com>
2023-08-20 22:38:35 -0300

    Entendendo função e objeto em typescript

--- aula1.js
+++ aula1.js
@@ -1,3 +1,25 @@
-"use strict";
-let numero = 10;
+/* DECLARANDO VARIAVEL EXISTEM 3 TIPOS: NUMBER, STRING, BOOLEAN*/
+var numero = 10;
 console.log(numero);
+/* DECLARANDO ARRAY */
+var array = [1, 2, 3, 4, 5];
+array.push(6);
+console.log(array[2]);
+/* outra forma de fazer array  */
+var array2 = [10, 9, 8];
+/* FUNÇÃO EM TYPESCRIPT */
+function soma(a, b) {
+    console.log(a, b);
+}
+soma(1, 2);
+/* RETORNO DA FUNÇÃO */
+function retornoSoma(c, d) {
+    return c + d;
+}
+console.log("esse \u00E9 o retorno da soma ".concat(retornoSoma(1, 2)));
+/* 35 - TIPOS DE OBJETO  */
+function objetoTipo(coord) {
+    console.log(coord.numero + '' + coord.nome);
+}
+var coord = { numero: 224, nome: 'jonathan' };
+objetoTipo(coord);
--- aula1.ts
+++ aula1.ts
@@ -6,4 +6,33 @@ console.log(numero)
 let array: number[] = [1,2,3,4,5]
 
 array.push(6)
-console.log(array)
+console.log(array[2])
+/* outra forma de fazer array  */
+let array2: Array<number> = [10,9,8]
+
+
+
+
+/* FUNÇÃO EM TYPESCRIPT */
+
+function soma(a:number, b:number){
+    console.log(a,b)
+}
+
+
+soma(1,2)
+
+/* RETORNO DA FUNÇÃO */ 
+function retornoSoma(c:number,d:number):number{
+    return c + d
+}
+
+console.log(`esse é o retorno da soma ${retornoSoma(1,2)}`)
+
+/* 35 - TIPOS DE OBJETO  */
+function objetoTipo(coord: {numero:number, nome:string}){
+    console.log(coord.numero + '' + coord.nome)
+}
+
+const coord = {numero: 224, nome: 'jonathan'}
+objetoTipo(coord)
